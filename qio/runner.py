70ed619912eab201c22773101b0e93cb98bae050
Ryan Hiebert <ryan@ryanhiebert.com>
2025-04-08 06:25:56 -0500

    Add a run command to wait on a single invocation
    Add a producer abstraction
    
    I'm not sure it's the right abstraction, maybe it just needs to be the broker, but some API like this needs to exist. It might need to be on the "app" class or whatever comes to wrap the commands that access the bus.

---
+++ qio/runner.py
@@ -0,0 +1,30 @@
+from collections.abc import Callable
+from typing import cast
+
+from .bus import Bus
+from .invocation import Invocation
+from .invocation import InvocationErrored
+from .invocation import InvocationSucceeded
+from .producer import Producer
+
+
+def run[R](invocation: Invocation[Callable[..., R]]) -> R:
+    """Submit and wait for an invocation to complete."""
+    bus = Bus()
+    producer = Producer()
+    completions = bus.subscribe({InvocationSucceeded, InvocationErrored})
+    producer.submit(invocation)
+    
+    try:
+        while True:
+            match completions.get():
+                case InvocationSucceeded() as event:
+                    if event.invocation.id == invocation.id:
+                        return cast(R, event.value)
+                case InvocationErrored() as event:
+                    if event.invocation.id == invocation.id:
+                        raise event.exception
+                case _:
+                    pass
+    finally:
+        bus.shutdown()
