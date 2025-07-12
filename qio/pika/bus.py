c3c4c41501617297cb516563cb8cbad04d9b3830
Ryan Hiebert <ryan@ryanhiebert.com>
2025-07-12 15:50:00 -0500

    Refactor the bus to make a transport
    A transport is the interface to the remote backend for the bus. It doesn't currently support filtering, although that could theoretically be added later. It doesn't know anything about types, and doesn't multiplex messages to subscribers. It interfaces with a remote transport so that events on other hosts are received.

--- qio/pika/bus.py
+++ qio/pika/bus.py
@@ -1,7 +1,11 @@
+from abc import ABC
+from abc import abstractmethod
 from collections.abc import Callable
 from collections.abc import Iterable
+from collections.abc import Iterator
 from itertools import chain
 from queue import Queue
+from queue import ShutDown
 from threading import Lock
 from threading import Thread
 from typing import Any
@@ -11,20 +15,68 @@ import dill
 from pika import BlockingConnection
 
 
+class BusTransport:
+    def __init__(self):
+        self.__subscriber = Queue[bytes]()
+        self.__subscribe_connection = BlockingConnection()
+        self.__subscribe_channel = self.__subscribe_connection.channel()
+        self.__queue_name = cast(
+            str, self.__subscribe_channel.queue_declare("", exclusive=True).method.queue
+        )
+        self.__subscribe_channel.queue_bind(self.__queue_name, "amq.topic", routing_key="#")
+        self.__subscribe_thread = Thread(target=self.__listen, name="qio-bus-transport-listener")
+        self.__subscribe_thread.start()
+
+        self.__lock = Lock()
+        self.__publish_channel = BlockingConnection().channel()
+
+    def __listen(self):
+        for _, _, body in self.__subscribe_channel.consume(self.__queue_name, auto_ack=True):
+            self.__subscriber.put(body)
+    
+    def subscribe(self) -> Iterator[bytes]:
+        while True:
+            try:
+                yield self.__subscriber.get()
+            except ShutDown:
+                return
+
+    def publish(self, message: bytes):
+        with self.__lock:
+            self.__publish_channel.basic_publish(
+                exchange="amq.topic",
+                routing_key="",
+                body=message,
+            )
+
+    def shutdown(self):
+        lock = Lock()
+        lock.acquire()
+
+        def callback():
+            self.__subscribe_channel.cancel()
+            lock.release()
+
+        self.__subscribe_connection.add_callback_threadsafe(callback)
+        with lock:
+            pass
+        
+        self.__subscribe_thread.join()
+        self.__subscriber.shutdown()
+
+
 class Bus:
     def __init__(self):
+        self.__transport = BusTransport()
         self.__subscriptions: dict[type, set[Queue[Any]]] = {}
-        self.__channel_lock = Lock()
-        self.__channel = BlockingConnection().channel()
-        self.__listener_lock = Lock()
-        self.__listener: Listener | None = None
+        self.__listener = Thread(target=self.__listen, name='qio-bus-listener')
+        self.__listener.start()
+    
+    def __listen(self):
+        for message in self.__transport.subscribe():
+            self.__remote_receive(message)
 
     def subscribe[T](self, types: Iterable[type[T]]) -> Queue[T]:
-        # Ensure that the listener is running
-        with self.__listener_lock:
-            if self.__listener is None:
-                self.__listener = Listener(self.__remote_receive)
-
         queue = Queue[T]()
         for type in types:
             self.__subscriptions.setdefault(type, set()).add(queue)
@@ -36,13 +88,6 @@ class Bus:
             subscriptions.discard(queue)
             if not subscriptions:
                 del self.__subscriptions[type]
-
-        # If there are no remaining subscriptions, shut down the listener
-        if not self.__subscriptions:
-            with self.__listener_lock:
-                if self.__listener is not None:
-                    self.__listener.shutdown()
-                    self.__listener = None
 
     def __distribute(self, event: Any):
         """Local-only distribution of events to subscribers."""
@@ -57,13 +102,7 @@ class Bus:
 
     def __remote_publish(self, event: Any):
         """Remote distribution of events to subscribers."""
-        body = dill.dumps(event)
-        with self.__channel_lock:
-            self.__channel.basic_publish(
-                exchange="amq.topic",
-                routing_key="#",
-                body=body,
-            )
+        self.__transport.publish(dill.dumps(event))
 
     def __remote_receive(self, body: bytes):
         """Receive and process remote events."""
@@ -71,54 +110,23 @@ class Bus:
         self.__distribute(event)
 
     def publish(self, event: Any):
-        """Publish to all bus subscribers.
+        """Publish an event to all subscribers of the bus transport.
 
-        Put on the backing event broker so that remote and local subscribers
+        Put on the bus transport so that remote and local subscribers
         see the event. Requires that the event is serializable.
         """
         self.__remote_publish(event)
 
     def publish_local(self, event: Any):
-        """Publish only to local subscribers.
+        """Publish only to subscribers of this Bus instance.
 
         This is useful for events that have properties that are not serializable.
         """
         self.__distribute(event)
 
     def shutdown(self):
-        with self.__listener_lock:
-            if self.__listener:
-                self.__listener.shutdown()
+        self.__transport.shutdown()
+        self.__listener.join()
         for subscriber in set(chain.from_iterable(self.__subscriptions.values())):
             subscriber.shutdown()
 
-
-class Listener:
-    def __init__(self, receive: Callable[[bytes], None], /):
-        self.__receive = receive
-        self.__connection = BlockingConnection()
-        self.__channel = self.__connection.channel()
-        self.__queue = cast(
-            str, self.__channel.queue_declare("", exclusive=True).method.queue
-        )
-        self.__channel.queue_bind(self.__queue, "amq.topic", routing_key="#")
-        self.__thread = Thread(target=self.__listen, name="qio-bus-listener")
-        self.__thread.start()
-
-    def shutdown(self):
-        lock = Lock()
-        lock.acquire()
-
-        def callback():
-            self.__channel.cancel()
-            lock.release()
-
-        self.__connection.add_callback_threadsafe(callback)
-        with lock:
-            pass
-
-        self.__thread.join()
-
-    def __listen(self):
-        for _, _, body in self.__channel.consume(self.__queue, auto_ack=True):
-            self.__receive(body)
